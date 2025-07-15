b0ae43021a52db54cf54a3d7d8df509446987785
João Justo <jpjustonunes@gmail.com>
2025-07-15 10:50:57 +0100

    Add gh pages deploy action

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://Siren-Farm.github.io',
+  base: 'siren-farm.com',
+})
