1001975a38dae9c43e7c146446136490cadc979e
João Justo <jpjustonunes@gmail.com>
2025-09-07 00:29:09 +0100

    First version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://Siren-Farm.github.io',
-  base: 'siren-farm.com',
-})
+  site: "https://Siren-Farm.github.io",
+  base: "siren-farm.com",
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
