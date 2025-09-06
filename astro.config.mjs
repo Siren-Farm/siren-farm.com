2692fe8cff109ace5718eb2212d26c2f88a2fcbe
João Justo <jpjustonunes@gmail.com>
2025-09-07 00:44:42 +0100

    Remove base from astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://siren-farm.com",
-  base: "siren-farm.com",
   vite: {
     plugins: [tailwindcss()],
   },
